bd7d57c1854dbd332c4bce4e09be72a6c3e4e399
WooYoungDoo <duden5000@naver.com>
2023-09-02 20:21:56 +0900

    initial commit

---
+++ src/main/java/tinqle/tinqleServer/TinqleServerApplication.java
@@ -0,0 +1,13 @@
+package tinqle.tinqleServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinqleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinqleServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tinqle/tinqleServer/TinqleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tinqle.tinqleServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinqleServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
