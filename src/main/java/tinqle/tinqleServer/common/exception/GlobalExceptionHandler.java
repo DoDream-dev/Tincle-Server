8e36284f29b9efe2b50377d9162caca19eec218b
WooYoungDoo <duden5000@naver.com>
2023-09-10 21:53:24 +0900

    feat: common exception

--- src/main/java/tinqle/tinqleServer/common/exception/GlobalExceptionHandler.java
+++ src/main/java/tinqle/tinqleServer/common/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tinqle.tinqleServer.common.dto.ApiResponse;
+import tinqle.tinqleServer.domain.account.exception.AuthException;
 
 @RestControllerAdvice
 @Slf4j
@@ -16,5 +17,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(ex.statusCode.getHttpCode())
                 .body(ApiResponse.error(ex.statusCode.getStatusCode(), ex.statusCode.getMessage()));
+    }
+
+    @ExceptionHandler(AuthException.class)  //분리 이유: SignToken
+    public ResponseEntity<?> handle(AuthException ex) {
+        log.warn("{}({}) - {}", ex.getClass().getSimpleName(), ex.statusCode.getStatusCode(), ex.getMessage());
+        return ResponseEntity
+                .status(ex.statusCode.getHttpCode())
+                .body(ApiResponse.error(ex.statusCode.getStatusCode(), ex.data, ex.statusCode.getMessage()));
     }
 }
