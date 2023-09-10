a14ef4deff7f71cb4c290eb508aeb82fe5f8e792
WooYoungDoo <duden5000@naver.com>
2023-09-10 21:55:03 +0900

    feat: getId 매서드 추가

--- src/main/java/tinqle/tinqleServer/config/security/PrincipalDetails.java
+++ src/main/java/tinqle/tinqleServer/config/security/PrincipalDetails.java
@@ -22,6 +22,10 @@ public class PrincipalDetails implements UserDetails {
         return authorities;
     }
 
+    public Long getId() {
+        return account.getId();
+    }
+
     @Override
     public String getPassword() {
         return account.getPassword();
