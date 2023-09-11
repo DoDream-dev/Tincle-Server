4fac55de7fd32b68d0db8dc815e967e85922ddc5
WooYoungDoo <duden5000@naver.com>
2023-09-11 22:53:50 +0900

    feat: constants

--- src/main/java/tinqle/tinqleServer/common/constant/GlobalConstants.java
+++ src/main/java/tinqle/tinqleServer/common/constant/GlobalConstants.java
@@ -1,6 +1,15 @@
 package tinqle.tinqleServer.common.constant;
 
+
 public class GlobalConstants {
+
+    /**
+     * AWS S3
+     */
+    public static final int S3_FILE_TYPE_MAX_LENGTH = 25;
+    public static final String FILE_EXTENSION_SEPARATOR = ".";
+    public static final String S3_OBJECT_NAME_PATTERN = "{0}_{1}.{2}";
+    public static final String ACCOUNT_STATUS_URL = "https://tinqle-s3.s3.ap-northeast-2.amazonaws.com/account";
 
     public static final String[] APPOINTED_URIS = {
             "/auth/reissue",
