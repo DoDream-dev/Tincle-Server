09b7f66ca3813bf98346f68be409dd2a60a7718c
WooYoungDoo <duden5000@naver.com>
2023-09-19 23:01:14 +0900

    feat: 계정 및 친구 더미 추가

---
+++ src/main/java/tinqle/tinqleServer/common/dummy/FriendshipDummy.java
@@ -0,0 +1,61 @@
+package tinqle.tinqleServer.common.dummy;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import tinqle.tinqleServer.domain.account.model.Account;
+import tinqle.tinqleServer.domain.account.repository.AccountRepository;
+import tinqle.tinqleServer.domain.friendship.model.Friendship;
+import tinqle.tinqleServer.domain.friendship.repository.FriendshipRepository;
+
+import java.util.List;
+
+@Component("friendshipDummy")
+@DependsOn("accountDummy")
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class FriendshipDummy {
+
+    private final FriendshipRepository friendshipRepository;
+    private final AccountRepository accountRepository;
+
+    @PostConstruct
+    public void init() {
+        if (friendshipRepository.count() > 0) {
+            log.info("[friendshipDummy] 친구 데이터가 이미 존재");
+        } else {
+            createFriendships();
+            log.info("[friendshipDummy] 친구 더미 생성 완료");
+        }
+    }
+
+    private void createFriendships() {
+        List<Account> accounts = accountRepository.findAll();
+
+        for (int i = 0; i < 50; i++) {
+            for (int j = i+1; j < 50; j++) {
+                Friendship friendshipA = Friendship.builder()
+                        .accountSelf(accounts.get(i))
+                        .accountFriend(accounts.get(j))
+                        .friendNickname("변경닉네임")
+                        .isChangeFriendNickname(true)
+                        .build();
+
+                Friendship friendshipB = Friendship.builder()
+                        .accountSelf(accounts.get(j))
+                        .accountFriend(accounts.get(i))
+                        .friendNickname("변경닉네임")
+                        .isChangeFriendNickname(true)
+                        .build();
+
+                friendshipRepository.save(friendshipA);
+                friendshipRepository.save(friendshipB);
+            }
+
+        }
+    }
+}
