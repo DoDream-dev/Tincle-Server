7b8febea2583dd892ada5035bbbf47219108a7c4
WooYoungDoo <duden5000@naver.com>
2023-09-19 23:11:46 +0900

    feat: 친구 관리 test controller

---
+++ src/main/java/tinqle/tinqleServer/domain/test/controller/TestController.java
@@ -0,0 +1,29 @@
+package tinqle.tinqleServer.domain.test.controller;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+import tinqle.tinqleServer.common.dto.ApiResponse;
+import tinqle.tinqleServer.domain.friendship.service.FriendshipService;
+
+import static tinqle.tinqleServer.common.dto.ApiResponse.success;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/test")
+@Hidden
+public class TestController {
+
+    private final FriendshipService friendshipService;
+
+    @GetMapping("/manage/{id}")
+    public ApiResponse<?> manage(
+            @PathVariable Long id,
+            @PageableDefault Pageable pageable,
+            @RequestParam(required = false) Long cursorId) {
+        return success(friendshipService.getFriendshipManage(id, pageable, cursorId));
+    }
+
+}
