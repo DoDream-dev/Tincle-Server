6bee0a97a7d9b46fcccdb22256f6eab0eefd867a
WooYoungDoo <duden5000@naver.com>
2023-09-22 16:47:59 +0900

    refactor: createdAt type 변경

--- src/main/java/tinqle/tinqleServer/common/model/BaseEntity.java
+++ src/main/java/tinqle/tinqleServer/common/model/BaseEntity.java
@@ -2,15 +2,12 @@ package tinqle.tinqleServer.common.model;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
@@ -18,24 +15,11 @@ import java.time.format.DateTimeFormatter;
 public abstract class BaseEntity {
 
     @CreatedDate
-    protected String createdAt;
+    protected LocalDateTime createdAt;
     @LastModifiedDate
     protected String modifiedAt;
     protected boolean visibility = true;
 
-    @PrePersist
-    protected void onPrePersist() {
-        createdAt = customFormat();
-        modifiedAt = createdAt;
-    }
-
-    @PreUpdate
-    protected void onPreUpdate() {
-        modifiedAt = customFormat();
-    }
-    private String customFormat() {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-    }
     public void softDelete() {
         visibility = false;
     }
