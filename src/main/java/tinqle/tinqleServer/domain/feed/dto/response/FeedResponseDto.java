5422c40b762d0e2a124e9d8cc0ae0ba7f6523395
WooYoungDoo <duden5000@naver.com>
2023-09-25 16:13:58 +0900

    refactor: feedId 응답 최상단

--- src/main/java/tinqle/tinqleServer/domain/feed/dto/response/FeedResponseDto.java
+++ src/main/java/tinqle/tinqleServer/domain/feed/dto/response/FeedResponseDto.java
@@ -11,11 +11,11 @@ import java.util.List;
 public class FeedResponseDto {
 
     public record FeedCardResponse(
+            Long feedId,
             Long accountId,
             String statusImageUrl,
             String nickname,
             String friendNickname,
-            Long feedId,
             String content,
             List<String> feedImageUrls,
             Long commentCount,
@@ -28,11 +28,11 @@ public class FeedResponseDto {
 
         public static FeedCardResponse of(Feed feed, String friendNickname, boolean isAuthor, EmoticonCountAndChecked emoticons) {
             return FeedCardResponse.builder()
+                    .feedId(feed.getId())
                     .accountId(feed.getAccount().getId())
                     .statusImageUrl(feed.getAccount().getStatus().getStatusImageUrl())
                     .nickname(feed.getAccount().getNickname())
                     .friendNickname(friendNickname)
-                    .feedId(feed.getId())
                     .content(feed.getContent())
                     .feedImageUrls(feed.getFeedImageList().stream().map(FeedImage::getImageUrl).toList())
                     .commentCount((long) feed.getCommentList().size())
