98547f6d78f7a3c0f43e44d32ac828c82a4fb140
WooYoungDoo <duden5000@naver.com>
2023-10-01 17:57:42 +0900

    feat: 12시간 후 피드 softDelete

--- src/main/java/tinqle/tinqleServer/domain/feed/repository/FeedRepository.java
+++ src/main/java/tinqle/tinqleServer/domain/feed/repository/FeedRepository.java
@@ -3,7 +3,11 @@ package tinqle.tinqleServer.domain.feed.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tinqle.tinqleServer.domain.feed.model.Feed;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 
 public interface FeedRepository extends JpaRepository<Feed, Long>, FeedRepositoryCustom {
 
+    List<Feed> findAllByCreatedAtBeforeAndVisibilityIsTrue(LocalDateTime beforeTime);
 }
