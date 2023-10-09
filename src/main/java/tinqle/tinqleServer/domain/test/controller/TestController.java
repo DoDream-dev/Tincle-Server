78532a06b4be57366f762706b10f921c923482a8
WooYoungDoo <duden5000@naver.com>
2023-10-09 19:22:25 +0900

    feat: 댓글 조회 테스트

--- src/main/java/tinqle/tinqleServer/domain/test/controller/TestController.java
+++ src/main/java/tinqle/tinqleServer/domain/test/controller/TestController.java
@@ -6,6 +6,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import tinqle.tinqleServer.common.dto.ApiResponse;
+import tinqle.tinqleServer.common.dto.SliceResponse;
+import tinqle.tinqleServer.domain.comment.dto.response.CommentResponseDto;
+import tinqle.tinqleServer.domain.comment.service.CommentService;
 import tinqle.tinqleServer.domain.emoticon.dto.response.EmoticonResponseDto;
 import tinqle.tinqleServer.domain.emoticon.service.EmoticonService;
 import tinqle.tinqleServer.domain.feed.service.FeedService;
@@ -22,6 +25,7 @@ public class TestController {
     private final FriendshipService friendshipService;
     private final FeedService feedService;
     private final EmoticonService emoticonService;
+    private final CommentService commentService;
 
     @GetMapping("/manage/{id}")
     public ApiResponse<?> manage(
@@ -45,4 +49,14 @@ public class TestController {
             @PathVariable Long accountId) {
         return success(emoticonService.getEmoticonReactAccount(accountId, feedId));
     }
+
+    @GetMapping("/{feedId}/comments/{accountId}")
+    public ApiResponse<SliceResponse<CommentResponseDto.CommentCardResponse>> getComments(
+            @PathVariable Long feedId,
+            @PathVariable Long accountId,
+            @PageableDefault Pageable pageable,
+            @RequestParam(required = false) Long cursorId) {
+        return success(commentService.getCommentsByFeed(accountId, feedId, pageable, cursorId));
+    }
+
 }
