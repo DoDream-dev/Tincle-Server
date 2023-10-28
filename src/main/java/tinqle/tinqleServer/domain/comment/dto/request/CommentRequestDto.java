31736c8ca535c14f760621a0ba11dc871d34d7e6
WooYoungDoo <duden5000@naver.com>
2023-10-28 15:08:07 +0900

    refactor: 빈칸 검증 추가

--- src/main/java/tinqle/tinqleServer/domain/comment/dto/request/CommentRequestDto.java
+++ src/main/java/tinqle/tinqleServer/domain/comment/dto/request/CommentRequestDto.java
@@ -1,12 +1,12 @@
 package tinqle.tinqleServer.domain.comment.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import static tinqle.tinqleServer.common.constant.ValidConstants.COMMENT_TEXT_LENGTH;
 
 public class CommentRequestDto {
     public record CreateCommentRequest(
-            @NotNull @Size(max = COMMENT_TEXT_LENGTH)
+            @NotBlank @Size(max = COMMENT_TEXT_LENGTH)
             String content) {}
 }
--- src/main/java/tinqle/tinqleServer/domain/feed/dto/request/FeedRequestDto.java
+++ src/main/java/tinqle/tinqleServer/domain/feed/dto/request/FeedRequestDto.java
@@ -1,6 +1,6 @@
 package tinqle.tinqleServer.domain.feed.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
@@ -10,7 +10,7 @@ import static tinqle.tinqleServer.common.constant.ValidConstants.FEED_TEXT_LENGT
 public class FeedRequestDto {
 
     public record CreateFeedRequest(
-            @NotNull @Size(max = FEED_TEXT_LENGTH)
+            @NotBlank @Size(max = FEED_TEXT_LENGTH)
             String content,
             List<String> feedImageUrl
     ) {}
