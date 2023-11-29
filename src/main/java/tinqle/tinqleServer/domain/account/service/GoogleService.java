df2619efc2e72fe0efdcbc5f858b2ecac8847161
WooYoungDoo <duden5000@naver.com>
2023-11-29 15:43:08 +0900

    feat: google 회원 탈퇴

--- src/main/java/tinqle/tinqleServer/domain/account/service/GoogleService.java
+++ src/main/java/tinqle/tinqleServer/domain/account/service/GoogleService.java
@@ -1,13 +1,16 @@
 package tinqle.tinqleServer.domain.account.service;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import tinqle.tinqleServer.domain.account.dto.GoogleDto.GoogleRefreshTokenResponse;
 import tinqle.tinqleServer.domain.account.dto.GoogleDto.GoogleTokenResponse;
 import tinqle.tinqleServer.domain.account.dto.response.AuthResponseDto.OAuthSocialEmailAndNicknameResponse;
 import tinqle.tinqleServer.domain.account.model.SocialType;
@@ -57,7 +60,8 @@ public class GoogleService {
         List<String> list = getGoogleIdAndName(responseBody.accessToken());
         log.info("Id={}", list.get(0));
         log.info("name={}", list.get(1));
-        return OAuthSocialEmailAndNicknameResponse.to(list.get(0), list.get(1));
+
+        return OAuthSocialEmailAndNicknameResponse.to(list.get(0), list.get(1), responseBody.refreshToken());
     }
 
     private MultiValueMap<String, String> generateTokenParams(final String authorizationCode) {
@@ -69,5 +73,37 @@ public class GoogleService {
         params.add("grant_type", "authorization_code");
         params.add("redirect_uri", redirectUri);
         return params;
+    }
+
+    protected boolean revokeGoogle(String refreshToken) {
+        String googleAccessToken = getGoogleAccessToken(refreshToken);
+        int revokeStatusCode = revoke(googleAccessToken);
+        return revokeStatusCode == 200;
+    }
+
+    @NotNull
+    private @JsonProperty("access_token") String getGoogleAccessToken(String refreshToken) {
+        String tokenUrl = "https://oauth2.googleapis.com/token";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("client_id", clientId);
+        params.add("client_secret", clientSecret);
+        params.add("grant_type", "refresh_token");
+        params.add("refresh_token", refreshToken);
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+
+        ResponseEntity<GoogleRefreshTokenResponse> response = restTemplate.postForEntity(tokenUrl, request, GoogleRefreshTokenResponse.class);
+        return response.getBody().accessToken();
+    }
+
+    private int revoke(String accessToken) {
+        String revokeUrl = "https://oauth2.googleapis.com/revoke";
+
+        MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
+        parameters.add("token", accessToken);
+        ResponseEntity<String> response = restTemplate.postForEntity(revokeUrl, parameters, String.class);
+        return response.getStatusCode().value();
     }
 }
