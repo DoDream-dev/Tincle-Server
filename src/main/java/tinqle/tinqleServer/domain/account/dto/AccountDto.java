fcb677d8aefa6307e54ab7bbdf91b1467a97df60
WooYoungDoo <duden5000@naver.com>
2023-11-29 15:48:23 +0900

    style: 코드 정리

--- src/main/java/tinqle/tinqleServer/domain/account/dto/AccountDto.java
+++ src/main/java/tinqle/tinqleServer/domain/account/dto/AccountDto.java
@@ -2,5 +2,7 @@ package tinqle.tinqleServer.domain.account.dto;
 
 public class AccountDto {
 
-    public record SigningAccount(String socialEmail, String socialUuid, String socialType, String nickname) {}
+    public record SigningAccount(String socialEmail, String socialUuid, String socialType, String refreshToken,
+                                 String nickname) {
+    }
 }
