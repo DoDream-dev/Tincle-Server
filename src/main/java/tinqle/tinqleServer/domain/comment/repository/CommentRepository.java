0aa1e97804347888d2385a3ee1e883c60d7c99af
WooYoungDoo <duden5000@naver.com>
2023-12-17 13:48:47 +0900

    feat: 대댓글 갯수 확인 쿼리문 추가

--- src/main/java/tinqle/tinqleServer/domain/comment/repository/CommentRepository.java
+++ src/main/java/tinqle/tinqleServer/domain/comment/repository/CommentRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import tinqle.tinqleServer.domain.comment.model.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+    Long countByParent(Comment parentComment);
 }
