d3a488c995de32be22bdb73b9117307f602637a2
WooYoungDoo <duden5000@naver.com>
2024-01-03 14:16:09 +0900

    test: test template 추가

--- src/test/java/tinqle/tinqleServer/domain/account/template/AccountTemplate.java
+++ src/test/java/tinqle/tinqleServer/domain/account/template/AccountTemplate.java
@@ -21,9 +21,11 @@ public class AccountTemplate {
     private static final SocialType SOCIAL_TYPE_B = SocialType.GOOGLE;
     private static final Role ROLE = Role.ROLE_USER;
     private static final boolean IS_RECEIVED_PUSH_NOTIFICATION = true;
+    private static final String IMAGE_URL_A = "imageUrlA";
+    private static final String IMAGE_URL_B = "imageUrlB";
     private static Account createAccount(Long id, String password, String nickname, String socialEmail,
                                          String code, AccountStatus accountStatus, Status status, SocialType socialType,
-                                         Role role, boolean isReceivedPushNotification) {
+                                         Role role, boolean isReceivedPushNotification, String profileImageUrl) {
         return Account.builder()
                 .id(id)
                 .password(password)
@@ -34,16 +36,50 @@ public class AccountTemplate {
                 .status(status)
                 .socialType(socialType)
                 .role(role)
-                .isReceivedPushNotification(isReceivedPushNotification).build();
+                .isReceivedPushNotification(isReceivedPushNotification)
+                .profileImageUrl(profileImageUrl).build();
+    }
+
+    // 인메모리 DB용 더미데이터는 ID 제거
+    private static Account createDummyAccountAExceptId(String password, String nickname, String socialEmail,
+                                                      String code, AccountStatus accountStatus, Status status, SocialType socialType,
+                                                      Role role, boolean isReceivedPushNotification, String profileImageUrl) {
+        return Account.builder()
+                .password(password)
+                .nickname(nickname)
+                .socialEmail(socialEmail)
+                .code(code)
+                .accountStatus(accountStatus)
+                .status(status)
+                .socialType(socialType)
+                .role(role)
+                .isReceivedPushNotification(isReceivedPushNotification)
+                .profileImageUrl(profileImageUrl).build();
     }
 
     public static Account createDummyAccountA() {
         return createAccount(ID_A, DUMMY_PASSWORD_A, DUMMY_NICKNAME_A, DUMMY_SOCIAL_EMAIL_A, CODE_A, ACCOUNT_STATUS,
-                STATUS_A, SOCIAL_TYPE_A,ROLE, IS_RECEIVED_PUSH_NOTIFICATION);
+                STATUS_A, SOCIAL_TYPE_A,ROLE, IS_RECEIVED_PUSH_NOTIFICATION, IMAGE_URL_A);
     }
     public static Account createDummyAccountB() {
         return createAccount(ID_B, DUMMY_PASSWORD_B, DUMMY_NICKNAME_B, DUMMY_SOCIAL_EMAIL_B, CODE_B, ACCOUNT_STATUS,
-                STATUS_B, SOCIAL_TYPE_B,ROLE, IS_RECEIVED_PUSH_NOTIFICATION);
+                STATUS_B, SOCIAL_TYPE_B,ROLE, IS_RECEIVED_PUSH_NOTIFICATION, IMAGE_URL_B);
+    }
+
+    public static Account createDummyAccountA_ExceptId() {
+        return createDummyAccountAExceptId(DUMMY_PASSWORD_A, DUMMY_NICKNAME_A, DUMMY_SOCIAL_EMAIL_A, CODE_A, ACCOUNT_STATUS,
+                STATUS_A, SOCIAL_TYPE_A,ROLE, IS_RECEIVED_PUSH_NOTIFICATION, IMAGE_URL_A);
+    }
+
+    public static Account createDummyAccountB_ExceptId() {
+        return createDummyAccountAExceptId( DUMMY_PASSWORD_B, DUMMY_NICKNAME_B, DUMMY_SOCIAL_EMAIL_B, CODE_B, ACCOUNT_STATUS,
+                STATUS_B, SOCIAL_TYPE_B,ROLE, IS_RECEIVED_PUSH_NOTIFICATION, IMAGE_URL_B);
+    }
+
+    public static Account createDummyAccountExceptId(String nickname) {
+        return Account.builder()
+                .nickname(nickname)
+                .build();
     }
 
 }
---
+++ src/test/java/tinqle/tinqleServer/domain/feed/template/FeedTemplate.java
@@ -0,0 +1,39 @@
+package tinqle.tinqleServer.domain.feed.template;
+
+import tinqle.tinqleServer.domain.account.model.Account;
+import tinqle.tinqleServer.domain.feed.model.Feed;
+
+public class FeedTemplate {
+
+    private static final Long ID_A = 1L;
+    private static final String CONTENT_A = "난 A입니다";
+
+    private static Feed createFeed(Long id, String content, Account account) {
+        return Feed.builder()
+                .id(id)
+                .content(content)
+                .account(account).build();
+    }
+
+    private static Feed createFeedExceptId(String content, Account account) {
+        return Feed.builder()
+                .content(content)
+                .account(account).build();
+    }
+
+
+    private static Feed createDummyFeedExceptId(String content, Account account) {
+        return Feed.builder()
+                .content(content)
+                .account(account).build();
+    }
+
+    public static Feed createDummyFeedA(Account account) {
+        return createFeed(ID_A, CONTENT_A, account);
+    }
+
+    public static Feed createDummyFeedAExceptId(Account account) {
+        return createFeedExceptId(CONTENT_A, account);
+    }
+}
+
