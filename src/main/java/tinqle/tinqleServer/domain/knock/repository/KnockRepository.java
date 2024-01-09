fdd4b1572df5ea8c126d5efb17498dda7a149293
WooYoungDoo <duden5000@naver.com>
2024-01-09 14:43:20 +0900

    feat: knockRepository 생성

---
+++ src/main/java/tinqle/tinqleServer/domain/knock/repository/KnockRepository.java
@@ -0,0 +1,13 @@
+package tinqle.tinqleServer.domain.knock.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tinqle.tinqleServer.domain.account.model.Account;
+import tinqle.tinqleServer.domain.knock.model.Knock;
+
+import java.util.List;
+
+public interface KnockRepository extends JpaRepository<Knock, Long> {
+    List<Knock> findAllByAccount(Account account);
+
+    boolean existsByAccountAndSendAccountAndVisibilityIsTrue(Account account, Account sendAccount);
+}
