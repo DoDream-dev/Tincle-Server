8c8b9f827459ae530ec37bb90f36855a401a7ef9
WooYoungDoo <duden5000@naver.com>
2024-01-10 16:21:41 +0900

    test: feed template 생성

--- src/test/java/tinqle/tinqleServer/domain/feed/template/FeedTemplate.java
+++ src/test/java/tinqle/tinqleServer/domain/feed/template/FeedTemplate.java
@@ -8,30 +8,32 @@ public class FeedTemplate {
     private static final Long ID_A = 1L;
     private static final String CONTENT_A = "난 A입니다";
 
-    private static Feed createFeed(Long id, String content, Account account) {
+    public static Feed createFeed(Long id, String content, Account account, boolean isKnock) {
         return Feed.builder()
                 .id(id)
                 .content(content)
-                .account(account).build();
+                .account(account)
+                .isKnock(isKnock).build();
     }
 
-    private static Feed createFeedExceptId(String content, Account account) {
+    private static Feed createFeedExceptId(String content, Account account, boolean isKnock) {
         return Feed.builder()
                 .content(content)
-                .account(account).build();
+                .account(account)
+                .isKnock(isKnock).build();
     }
 
 
     public static Feed createDummyFeedExceptId(String content, Account account) {
-        return createFeedExceptId(content, account);
+        return createFeedExceptId(content, account, false);
     }
 
     public static Feed createDummyFeedA(Account account) {
-        return createFeed(ID_A, CONTENT_A, account);
+        return createFeed(ID_A, CONTENT_A, account, false);
     }
 
     public static Feed createDummyFeedAExceptId(Account account) {
-        return createFeedExceptId(CONTENT_A, account);
+        return createFeedExceptId(CONTENT_A, account, false);
     }
 }
 
