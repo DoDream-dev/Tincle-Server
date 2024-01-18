84d605beb23aa8cf2df536be69536c95374f4a39
WooYoungDoo <duden5000@naver.com>
2024-01-18 23:31:52 +0900

    feat: 댓글에 이모티콘 반응 스웨거 추가

--- src/main/java/tinqle/tinqleServer/common/constant/SwaggerConstants.java
+++ src/main/java/tinqle/tinqleServer/common/constant/SwaggerConstants.java
@@ -82,7 +82,8 @@ public class SwaggerConstants {
 
     public static final String TAG_EMOTICON = "Emoticon";
     public static final String TAG_EMOTICON_DESCRIPTION = "Emoticon API";
-    public static final String EMOTICON_REACT = "이모티콘 반응";
+    public static final String EMOTICON_REACT_ON_FEED = "피드에 이모티콘 반응";
+    public static final String EMOTICON_REACT_ON_COMMENT = "댓글에 이모티콘 반응";
     public static final String EMOTICON_GET_REACTION_ACCOUNT = "이모티콘 반응한 사람 조회";
 
     /**
