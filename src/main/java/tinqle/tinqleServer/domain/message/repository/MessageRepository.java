b9fc5815feab79ec8898b2e6dbe666b3f35037dd
WooYoungDoo <duden5000@naver.com>
2024-03-01 17:41:34 +0900

    feat: 메세지 최근 순 조회

--- src/main/java/tinqle/tinqleServer/domain/message/repository/MessageRepository.java
+++ src/main/java/tinqle/tinqleServer/domain/message/repository/MessageRepository.java
@@ -1,7 +1,13 @@
 package tinqle.tinqleServer.domain.message.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import tinqle.tinqleServer.domain.account.model.Account;
 import tinqle.tinqleServer.domain.message.model.Message;
+import tinqle.tinqleServer.domain.room.model.Room;
 
-public interface MessageRepository extends JpaRepository<Message, Long> {
+import java.util.List;
+
+public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
+
+    List<Message> findAllByReceiverAndRoomAndIsReadFromReceiverIsFalse(Account receiver, Room room);
 }
