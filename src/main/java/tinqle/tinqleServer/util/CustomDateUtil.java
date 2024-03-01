623ff6cd1e57b672fbe1847a0ad6fbfa5d357bb4
WooYoungDoo <duden5000@naver.com>
2024-03-02 01:42:50 +0900

    feat: status 경과 시간 format

--- src/main/java/tinqle/tinqleServer/util/CustomDateUtil.java
+++ src/main/java/tinqle/tinqleServer/util/CustomDateUtil.java
@@ -17,6 +17,17 @@ public class CustomDateUtil {
         else return localDateTime.format(DateTimeFormatter.ofPattern("M월 dd일"));
     }
 
+    public static String resolveStatusElapsedTime(LocalDateTime localDateTime) {
+        LocalDateTime now = LocalDateTime.now();
+
+        Duration between = Duration.between(localDateTime, now);
+
+        if (between.toMinutes() == 0) return "방금";
+        else if (between.toHours() == 0) return "%d분".formatted(between.toMinutes());
+        else if (between.toDays() == 0) return "%d시간".formatted(between.toHours());
+        else return "%d일".formatted(between.toDays());
+    }
+
     public static String resolveDateFromDateTime(LocalDateTime localDateTime) {
         LocalDateTime now = LocalDateTime.now();
 
