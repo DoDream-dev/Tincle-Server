f06b7ff5e25b0e71755a158957465d090927aadf
WooYoungDoo <duden5000@naver.com>
2024-03-18 16:18:34 +0900

    refactor: 방 나가기 시 bulk update 및 카운트, 조회 매서드

--- src/main/java/tinqle/tinqleServer/domain/message/repository/MessageRepositoryCustom.java
+++ src/main/java/tinqle/tinqleServer/domain/message/repository/MessageRepositoryCustom.java
@@ -2,9 +2,13 @@ package tinqle.tinqleServer.domain.message.repository;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import tinqle.tinqleServer.domain.account.model.Account;
 import tinqle.tinqleServer.domain.message.model.Message;
+import tinqle.tinqleServer.domain.room.model.Room;
 
 public interface MessageRepositoryCustom {
 
-    Slice<Message> findByRoomSortRecently(Long roomId, Pageable pageable, Long cursorId);
+    Slice<Message> findByRoomSortRecently(Long roomId, Pageable pageable, Long cursorId, boolean isStarter);
+    void deleteAllMessageWhenStarter(Account account, Room room);
+    void deleteAllMessageWhenFriend(Account account, Room room);
 }
